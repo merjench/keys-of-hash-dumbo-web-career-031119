7f121ddf1acf0038f52ed55c6697c2209512af09
Merjen Charyyeva <merjen.chary@gmail.com>
2019-02-28 02:21:46 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,19 @@
-class Hash
-  def keys_of(arguments)
+# class Hash
+#  def keys_of(arguments)
     # code goes here
 
-   end	  
-  end
+#  end	  
+#  end
+  
+class Hash
+  def keys_of(*arguments)
+    # code goes here	    # code goes here
+		animals = []
+		self.each { |animal, place|
+			if arguments.include?(place)
+				animals << animal
+			end
+		}
+		return animals
+  end	  end
+end 	end
